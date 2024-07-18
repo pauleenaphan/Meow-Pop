5d99823d35e5f229eccf384e8f59b825bbe1b39e
pauleena phan <pauleena2002@gmail.com>
2024-07-17 22:11:22 -0700

    added more routes for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from '../src/pages/Home';
-import { Navbar } from './components/Navbar';
+import { Login } from '../src/pages/Login.tsx';
+import { Signup } from '../src/pages/Signup.tsx';
+import { Account } from '../src/pages/Account.tsx';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </Router>
   );
 }
