5f4608587e0fb9228da754f71f3d919d32447977
pauleena phan <pauleena2002@gmail.com>
2024-07-21 23:29:51 -0700

    removed icons for now

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,21 +1,21 @@
 // import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faMagnifyingGlass, faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
 import "../styles/nav.css";
 
 export const Header = () =>{
-    const navigate = useNavigate();
-    const isLoggedIn = localStorage.getItem('isLogged') === 'true';
+    // const navigate = useNavigate();
+    // const isLoggedIn = localStorage.getItem('isLogged') === 'true';
 
-    const handleUserIconClick = () => {
-        if (isLoggedIn) {
-            navigate("/account");
-        } else {
-            navigate("/login");
-        }
-    };
+    // const handleUserIconClick = () => {
+    //     if (isLoggedIn) {
+    //         navigate("/account");
+    //     } else {
+    //         navigate("/login");
+    //     }
+    // };
 
     return(
         <header> 
@@ -24,11 +24,11 @@ export const Header = () =>{
                     <a className="title" href="/">MeowPop</a>
                     <p className="caption">Everything your cat deserves</p>
                 </div>
-                <div className="icons">
+                {/* <div className="icons">
                     <FontAwesomeIcon icon={faMagnifyingGlass}/>
                     <FontAwesomeIcon icon={faCartShopping}/>
                     <FontAwesomeIcon icon={faUser} onClick={handleUserIconClick} />
-                </div>
+                </div> */}
             </section>
         </header>
     )
