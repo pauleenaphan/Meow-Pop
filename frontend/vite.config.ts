76bfe34ead288fe03f2436ebc25c342d2c7492d1
pauleena phan <pauleena2002@gmail.com>
2024-07-26 19:19:37 -0700

    added base: / to vite.config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this is set to root for deployment
   build: {
     outDir: 'dist', // Output directory for the build
     assetsDir: 'assets', // Directory for static assets within the outDir
