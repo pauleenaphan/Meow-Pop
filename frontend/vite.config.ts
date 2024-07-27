0021358079b81892caf403acfc22e23be0f0d917
pauleena phan <pauleena2002@gmail.com>
2024-07-26 19:10:48 -0700

    changed configs for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Output directory for the build
+    assetsDir: 'assets', // Directory for static assets within the outDir
+    rollupOptions: {
+      input: {
+        main: './index.html', // Main entry point for your application
+      },
+    },
+    minify: true, // Enable minification
+    target: 'es2015', // Target JavaScript version
+    sourcemap: true, // Generate source maps
+  },
+});
