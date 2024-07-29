6cdc648989257dae7f9039f9b230e2c626ec7cf0
pauleena phan <pauleena2002@gmail.com>
2024-07-29 11:58:13 -0700

    changed edititem route to put to patch

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -7,7 +7,7 @@ const { upload, uploadMiddleware } = require('../middleware/fileUpload');
 router.post('/createProduct/:id', authenticateJWT, upload.array('images', 10), productController.createProduct); 
 router.get('/getProduct/:id', productController.getProduct);
 router.get('/getAllProducts', productController.getAllProducts);
-router.patch('/editProduct/:id', authenticateJWT, uploadMiddleware, productController.editProduct); 
+router.put('/editProduct/:id', authenticateJWT, uploadMiddleware, productController.editProduct); 
 router.delete('/deleteProduct/:productId/:vendorId', authenticateJWT, productController.deleteProduct);
 
 module.exports = router;
